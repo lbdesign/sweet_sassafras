842e2c35bd3002809c983a3b92b0be71d24a665b
Liam Dempsey <liam@lbdesign.tv>
2014-02-04 16:30:28 -0500

    Updated Sass and other files

--- footer.php
+++ footer.php
@@ -12,8 +12,8 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php do_action( 'sweet_sassafras_credits' ); ?>
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'sweet-sassafras' ), 'WordPress' ); ?></a>
+			&copy; 
+			<a href="<?php bloginfo ('site_url'); ?>"><?php bloginfo ('site_name'); ?></a>, all rights reserved.
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'sweet-sassafras' ), 'Sweet Sassafras', '<a href="http://liamdempsey.com" rel="designer">Liam Dempsey</a>' ); ?>
 		</div><!-- .site-info -->
